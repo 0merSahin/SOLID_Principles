75a0bdadf1c2aebb08288dee6a329067d145ea2b
Ömer Şahin <121351361+0merSahin@users.noreply.github.com>
2023-06-03 16:09:17 +0300

    Add files via upload

---
+++ LSP_Project/LSP_Project/LSP_Classes/PriceType2_LSP_13.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace SolidProject1_211229013.LSP_Classes
+{
+	public class PriceType2_LSP_13 : IPrice_LSP_13, ICargo_LSP_13
+    {
+        public int Price { get; set; }
+        public int CargoPrice { get; set; }
+
+        public int CalculatePrice(int counter)
+        {
+            Price = counter * 20;
+            Price = AddCargoPrice(Price, counter);
+            Console.WriteLine("Each product is 20 TL");
+            return Price;
+        }
+
+        public int AddCargoPrice(int Price, int counter)
+        {
+            return Price + (10 * counter);
+        }
+    }
+}
+
---
+++ LSP_Project/LSP_Project/LSP_Classes/Products_LSP_13.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace SolidProject1_211229013.LSP_Classes
+{
+	public class Products_LSP_13
+    {
+        public List<int> ProductsIDList = new List<int>();
+
+		public Products_LSP_13()
+		{
+			if (!FileService_LSP_13.FileControl("ProductsIDList.txt")) return;
+			ProductsIDList = FileService_LSP_13.ReadingFile("ProductsIDList.txt");
+		}
+
+        public bool ProductControl(List<int> OrderList)
+        {
+            foreach (var item1 in OrderList)
+            {
+                bool control = false;
+                foreach (var item2 in ProductsIDList)
+                {
+                    if (item1 == item2)
+                    {
+                        control = true;
+                        break;
+                    }
+                }
+                if (control == false)
+                {
+                    Console.WriteLine("The product in your product list was not found! \nTransaction Canceled!");
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
+
